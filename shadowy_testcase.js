3444719afd009e2dba9138960468aba9726643ca
Adrian Aichner <adrian.aichner@gmail.com>
2017-11-29 14:20:52 +0100

    Name testcase somewhat appropriately. Add :host CSS test.

--- shadowy_testcase.js
+++ shadowy_testcase.js
@@ -1,13 +1,9 @@
 'use strict';
 
 function getAllPropertyNames(obj, props = []) {
-  // console.log(obj.constructor.name, props);
   if (obj.constructor.name == 'Object') {
-    // console.log(obj.constructor.name, props);
     return props.length ? props : null;
-    // return props;
   } else {
-    // console.log(obj, props);
     return getAllPropertyNames(Object.getPrototypeOf(obj), props.concat(Object.getOwnPropertyNames(obj)));
   }
 }
@@ -20,8 +16,6 @@ function reportError(error) {
 class Jot extends HTMLElement {
   constructor() {
     try {
-      // Trying to extent HTMLPreElement I get:
-      // VM1330:1 Uncaught TypeError: Illegal constructor
       super();
       this._shadow = this.attachShadow({ mode: 'open' });
       // this._shadow = this;
@@ -31,14 +25,17 @@ class Jot extends HTMLElement {
         <pre contenteditable="true">
         Jot
       </pre>
-        <textarea placeholder="typing here crashes browser tab"></textarea>
-        <input type="text" placeholder="On Android characters get duplicated">
+        <textarea placeholder="typing crashes tab on linux"></textarea>
+        <input type="text" placeholder="characters get duplicated on android">
         <div contenteditable="true">
         </div>
         </section>
         <!--
       -->
         <style>
+        :host pre::before {
+          content: " :host pseudo-class IS now supported in this browser!"
+        }
         /* :host pseudo-class not supported in firefox yet,
            using section as a workaround. See also
            https://bugzilla.mozilla.org/show_bug.cgi?id=992245
@@ -49,12 +46,9 @@ class Jot extends HTMLElement {
         position: fixed;
         top: 25%;
         left: 25%;
-        /*width: 50%;
-          height: 50%;*/
         z-idex: 1000;
       }
       span {
-        /* display: block; */
         right: 0;
         top: 0;
         position: absolute;
@@ -65,63 +59,14 @@ class Jot extends HTMLElement {
       this._pre = this._shadow.querySelector('pre');
       this._ta = this._shadow.querySelector('textarea');
       this._div = this._shadow.querySelector('div');
-      this._div.textContent = 'type here';
-      this._div.addEventListener('input', event => {
-        // event.preventDefault();
-        // event.stopPropagation();
-      }, true);
-      this._div.addEventListener('input', event => {
-        // event.preventDefault();
-        // event.stopPropagation();
-        console.log(event);
-      }, false);
-      this._div.addEventListener('keydown', event => {
-        // event.preventDefault();
-        // event.stopPropagation();
-        console.log(event);
-      }, true);
-      this._div.addEventListener('keydown', event => {
-        // event.preventDefault();
-        // event.stopPropagation();
-        console.log(JSON.stringify(event, null/*[
-          'currentTarget',
-          'target',
-          'type',
-          'eventPhase'
-        ]*/, 2));
-        console.log(event);
-      }, false);
-      this._div.addEventListener('blur', event => {
-        // event.preventDefault();
-        // event.stopPropagation();
-        console.log(event);
-      }, false);
-      this._div.addEventListener('focus', event => {
-        // event.preventDefault();
-        // event.stopPropagation();
-        console.log(event);
-      }, false);
-      this.addEventListener('blur', event => {
-        // event.preventDefault();
-        // event.stopPropagation();
-        console.log(event);
-      }, false);
-      this.addEventListener('focus', event => {
-        // event.preventDefault();
-        // event.stopPropagation();
-        console.log(event);
-      }, false);
+      this._div.textContent = 'characters do not get inserted';
       this._pre.addEventListener('focus', event => {
-        console.log(JSON.stringify(event, null/*[
-          'currentTarget',
-          'target',
-          'type',
-          'eventPhase'
-        ]*/, 2));
         let s = window.getSelection();
         s.selectAllChildren(event.target);
       });
-      this._close.addEventListener('click', e => { this.parentElement.removeChild(this); });
+      this._close.addEventListener('click', e => {
+        this.parentElement.removeChild(this);
+      });
     }
     catch(e) {
       reportError(e);
