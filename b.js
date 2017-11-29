0ad396671dccb69580847a863bf19d34164fda1e
Adrian Aichner <adrian.aichner@gmail.com>
2017-11-29 18:28:53 +0100

    catch and report errors in b.js

--- b.js
+++ b.js
@@ -1,39 +1,51 @@
 'use strict';
 
+import { infojs } from './info-module.js';
+
 let b = 2;
 
 class Jot extends HTMLElement {
   constructor() {
-    // Trying to extent HTMLPreElement I get:
-    // VM1330:1 Uncaught TypeError: Illegal constructor
-    super();
-    this._shadow = this.attachShadow({ mode: 'open' });
-    this._shadow.innerHTML = `
-      <pre contenteditable="true">
-      Jot
+    try {
+      // Trying to extent HTMLPreElement I get:
+      // VM1330:1 Uncaught TypeError: Illegal constructor
+      super();
+      this._shadow = this.attachShadow({ mode: 'open' });
+      this._shadow.innerHTML = `
+        <pre contenteditable="true">
+        Jot
       </pre>
-      <textarea></textarea>
-      <style>
-      pre {
-        opacity: 0.8;
-        position: fixed;
-        top: 25%;
-        left: 25%;
-        /*width: 50%;
-          height: 50%;*/
-        z-idex: 1000;
-      }
-    </style>
-      `;
+        <textarea></textarea>
+        <style>
+        pre {
+          opacity: 0.8;
+          position: fixed;
+          top: 25%;
+          left: 25%;
+          /*width: 50%;
+            height: 50%;*/
+          z-idex: 1000;
+        }
+      </style>
+        `;
+    }
+    catch(e) {
+      infojs(e, document.body);
+    }
   }
   connectedCallback() {
-    this._pre = this._shadow.firstElementChild;
-    this._pre.textContent = `# ${document.title}\n## ${document.URL}\n\n${window.getSelection().toString()}\n`;
-    this._pre.addEventListener('focus', e => {
-      let s = window.getSelection();
-      s.selectAllChildren(e.currentTarget);
-    });
-    this._pre.addEventListener('blur', e => { this.parentElement.removeChild(this); });
+    try {
+      this._pre = this._shadow.firstElementChild;
+      this._pre.textContent = `# ${document.title}\n## ${document.URL}\n\n${window.getSelection().toString()}\n`;
+      this._pre.addEventListener('focus', e => {
+        let s = window.getSelection();
+        s.selectAllChildren(e.currentTarget);
+      });
+      this._pre.addEventListener('blur', e => { this.parentElement.removeChild(this); });
+    }
+    catch(e) {
+      infojs(e, document.body);
+    }
   }
 }
 
